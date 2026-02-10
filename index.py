1b624513f3bf1e4449bda946a5efcc7891d529e1
AzhaganNJ <azhagannatrajan2006@gmail.com>
2026-02-10 22:39:03 +0530

    Some change

--- index.py
+++ index.py
@@ -1,17 +1,1 @@
-import sys
-import os
-
-# Add current directory to path
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-if BASE_DIR not in sys.path:
-    sys.path.insert(0, BASE_DIR)
-
-# Debug: Print path to see if app folder is visible
-print(f"Current Path: {sys.path}")
-print(f"Directory Contents: {os.listdir(BASE_DIR)}")
-
-try:
-    from app.main import app
-except Exception as e:
-    print(f"Import Error Details: {e}")
-    raise e
+from app.main import app
