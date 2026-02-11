3081b57cbced3fecd225e69f3dc87636328cf5f1
AzhaganNJ <azhagannatrajan2006@gmail.com>
2026-02-11 09:45:56 +0530

    changed database.py file for vercel

--- app/database.py
+++ app/database.py
@@ -2,16 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-# Use single DATABASE_URL from Vercel environment variables
 DATABASE_URL = os.getenv("DATABASE_URL")
-
-if not DATABASE_URL:
-    raise ValueError("DATABASE_URL is not set in environment variables.")
 
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True,
-    connect_args={"sslmode": "require"}  # Required for Supabase
+    connect_args={"sslmode": "require"}
 )
 
 SessionLocal = sessionmaker(
