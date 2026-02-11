759844781a723c9e35eb49341753a6937e7bbba1
AzhaganNJ <azhagannatrajan2006@gmail.com>
2026-02-11 09:30:17 +0530

    testing deployment in vercel

--- app/main.py
+++ app/main.py
@@ -1,37 +1,45 @@
+# from fastapi import FastAPI
+# from fastapi.middleware.cors import CORSMiddleware
+# from app.database import Base, engine
+# from app.routers.auth import router as auth
+
+# from app.routers.rooms import router as room
+# from app.routers.questions import router as question
+# from app.routers.votes import router as vote
+
+# # creating the database tables
+# Base.metadata.create_all(bind=engine)
+
+# app = FastAPI(title="Questup Backend")
+
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+# app.include_router(auth)
+# app.include_router(room)
+# app.include_router(question)
+# app.include_router(vote)
+
+
+# @app.get("/")
+# def home():
+#     return {"Questup": "Api is running successfully!!!"}
+
+
+# @app.get("/health")
+# def health():
+#     return {"status": "Running Sucesfully!!!"}
+
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from app.database import Base, engine
-from app.routers.auth import router as auth
 
-from app.routers.rooms import router as room
-from app.routers.questions import router as question
-from app.routers.votes import router as vote
-
-# creating the database tables
-Base.metadata.create_all(bind=engine)
-
-app = FastAPI(title="Questup Backend")
-
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-app.include_router(auth)
-app.include_router(room)
-app.include_router(question)
-app.include_router(vote)
-
+app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"Questup": "Api is running successfully!!!"}
-
-
-@app.get("/health")
-def health():
-    return {"status": "Running Sucesfully!!!"}
+    return {"message": "Vercel Working 🚀"}
